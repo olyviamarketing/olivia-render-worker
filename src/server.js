2b91b2df26d98c17c659c47ec3722f51f75311d7
olyviamarketing <olyviamarketing@gmail.com>
2026-09-09 16:20:15 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -177,7 +177,7 @@ const server = http.createServer(async (req, res) => {
     return json(res, version ? 200 : 503, {
       ok: Boolean(version),
       service: 'olivia-render-worker',
-      worker: 'V126-TEXT-WRAP',
+      worker: 'V127-TEXT-WRAP-RUNTIME-FIX',
       ffmpeg: version
     });
   }
